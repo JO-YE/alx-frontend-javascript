5125cecf1b052654182fc36460b94f80d4db79eb
JO-YE <JO-YE@ainaoluwadamilolaracheal.com>
2023-06-27 14:03:41 +0100

    modifying fxn by passing a rest param

---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,8 @@
+// returing the number of args passed to a rest param
+
+export default function returnHowManyArguments(...args) {
+	let count = 0;
+	for (let arg of args)
+		count += 1;
+	return count;
+};
