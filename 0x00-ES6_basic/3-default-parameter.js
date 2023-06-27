372f24f369757b94eb6d8935a977635b0b98dc38
JO-YE <JO-YE@ainaoluwadamilolaracheal.com>
2023-06-27 13:37:52 +0100

    assigning default values to params

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,6 @@
+// Condense the internals of the following function to 1 line
+// without changing the name of each function/variable.
+
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+	  return initialNumber + expansion1989 + expansion2019;
+}
