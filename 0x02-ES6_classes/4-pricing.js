24bb39b3c9a6bbe7254b021e233d28d5dc1e3ba1
JO-YE <JO-YE@ainaoluwadamilolaracheal.com>
2023-06-30 00:54:08 +0100

    adding a static method and getting attribute value for the import

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from "./3-currency.js";
+
+export default class Pricing {
+constructor(amount = 0, currency = null) {
+  this._amount = amount;
+  this._currency = currency;
+}
+
+  get amount() {
+    return this._amount;
+  }
+
+  set code(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set name(newCurrency) {
+    if (!(newCurency instanceof Currency)) {
+      throw new TypeError('Not Currency');
+    }
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount = 0, convertionRate = 0) {
+    return amount * convertionRate
+  }
+}
