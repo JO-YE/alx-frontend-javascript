fcc6562c401abf2d17a3c4d1767566c2ddae6376
JO-YE <JO-YE@ainaoluwadamilolaracheal.com>
2023-06-30 11:22:49 +0100

    indentation error

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,8 +1,7 @@
-// returing the number of args passed to a rest param
 export default function returnHowManyArguments(...args) {
+// returing the number of args passed to a rest param
   let count = 0;
-  for (let arg of args) {
+  for (let arg of args)
     count += 1;
-    return count;
-  }
+  return count;
 };
