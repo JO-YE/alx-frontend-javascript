d2009c7a1405926da140dfa839215b1a185bd732
JO-YE <JO-YE@ainaoluwadamilolaracheal.com>
2023-06-30 09:38:09 +0100

    changing the last export to default

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -13,7 +13,6 @@ export class HolbertonClass {
   }
 }
 
-
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -36,7 +35,6 @@ export class StudentHolberton {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
-
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
