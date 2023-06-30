24f660b44dc9b6deb06f0e95b3e20988d958ed53
JO-YE <JO-YE@ainaoluwadamilolaracheal.com>
2023-06-30 10:00:37 +0100

    indentation error, changed to single quote, inputting missing colon

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,14 @@
-import Car from "./10-car.js";
+import Car from './10-car';
 
 export default class EVCar extends Car {
-constructor(brand = '', motor = '', color = '', range = '') {
-  super(brand, motor, color);
-  this._range = range;
+  constructor(brand = '', motor = '', color = '', range = '') {
+    super(brand, motor, color);
+    this._range = range;
   }
 
   cloneCar() {
-  const clonedCar = new Car(this._brand, this._motor, this._color);
-  return clonedCar
+    const clonedCar = new Car(this._brand, this._motor, this._color);
+    return clonedCar;:wq
+
   }
 }
