c609aa79de99f99d136b590567bb68a079aca4c6
JO-YE <JO-YE@ainaoluwadamilolaracheal.com>
2023-06-30 09:49:25 +0100

    indentation error and removing unused variable

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,16 +1,11 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
 export default class Car {
-constructor(brand = '', motor = '', color = '') {
-  this._brand = brand;
-  this._motor = motor;
-  this._color = color;
+  constructor(brand = '', motor = '', color = '') {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   cloneCar() {
-  const clonedCar = Object.assign(Object.create(Object.getPrototypeOf(this)), this);
-  return clonedCar
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
