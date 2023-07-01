62f6456dd3a159fa8b3d3387f87bf4140b60ad78
JO-YE <JO-YE@ainaoluwadamilolaracheal.com>
2023-07-01 17:03:05 +0100

    sorting out lint errors

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,23 +1,11 @@
-/*export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+	const task2 = true;
 
   if (trueOrFalse) {
     const task = false;
     const task2 = true;
   }
     
-  return [task, task2];
-}
-*/
-export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
-
-  if (trueOrFalse) {
-    var taskIf = true;
-    var task2If = false;
-  }
-
   return [task, task2];
 }
